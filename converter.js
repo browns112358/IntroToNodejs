e8201af78a65a5ec10db44d83b6650127a0cb036
browns112358 <browns1123581321@gmail.com>
2018-10-02 09:20:29 -0400

    module1 submission

---
+++ converter.js
@@ -0,0 +1,10 @@
+const csvtojson = require('csvtojson');
+const path = require('path');
+const fs = require('fs');
+
+const csvFilePath = path.join(__dirname, 'customer-data.csv');
+csvtojson()
+    .fromFile(csvFilePath)
+    .then((jsonObj) => {
+        fs.writeFileSync(path.join(__dirname, 'customer-data.json'), JSON.stringify(jsonObj), 'utf-8');
+    });
